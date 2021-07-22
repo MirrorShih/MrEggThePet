a5c7dee68a9017232d9a4474ca7e9852d84315b1
Mirror <jess2529156651@gmail.com>
2021-07-22 17:28:05 +0800

    feat: move it to tray and delete ctrl

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 
+var tray = null
+
 function createWindow() {
-    // Create the browser window.
     const mainWindow = new BrowserWindow({
         width: 140,
         height: 145,
@@ -11,24 +12,48 @@ function createWindow() {
         },
         frame: false,
         transparent: true,
-        autoHideMenuBar: true,
+        autoHideMenuBar: true
     });
 
     mainWindow.loadFile('index.html');
     mainWindow.setAlwaysOnTop(true, 'floating')
+    mainWindow.setSkipTaskbar(true)
     return mainWindow;
 }
 
+
+function createTray(win) {
+
+    const switchMrEgg = (eggNo) => () => {
+        win.show();
+        win.webContents.send('switch-mregg', eggNo);
+    }
+    const iconPath = path.join(__dirname, './imgs/tray.png');
+    tray = Tray(iconPath)
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Egg 0', click: switchMrEgg(1) },
+        { label: 'Egg Walk', click: switchMrEgg(2) },
+        {
+            label: 'Hide',
+            click: () => win.hide()
+        },
+        {
+            label: 'Close',
+            click: () => {
+                app.isQuiting = true;
+                app.quit();
+            }
+        }
+    ])
+    tray.setToolTip("MrEggThePet")
+    tray.setContextMenu(contextMenu);
+    tray.on('click', () => win.show())
+    return tray;
+}
+
 app.on('ready', () => {
-
     const win = createWindow();
-
-    [1, 2].map(number => {
-
-        globalShortcut.register(`CommandOrControl+${number}`, () => {
-            win.webContents.send('switch-mregg', number);
-        })
-    })
+    createTray(win);
 })
 
 app.on('window-all-closed', () => app.quit())
